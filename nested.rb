81ad03d391c1236fea156eabe614b7912ec6f61a
joshua richards <jermain119@gmail.com>
2019-09-25 19:40:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def adding_matz
@@ -79,7 +80,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages][0]
     
 end
 
@@ -101,10 +102,10 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+programmer_hash[:alan_kay][:alans_new_info] = programmer_hash[:alan_kay].delete(:known_for)
+programmer_hash[:alan_kay][:alans_new_info] = "GUI"
+p programmer_hash
 end
-
 def adding_to_dennis
 	programmer_hash = 
  		{
@@ -121,5 +122,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+      programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+     
+     programmer_hash
 
 end
